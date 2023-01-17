eccd2a8fe9ea18e229342dd1da6a1c8e2de5fb2c
Rodrigo Torres de Souza <rtsouza87@gmail.com>
2023-01-17 20:05:05 -0300

    Primeiro commit

---
+++ Atividade1/backup-postgresql.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import psycopg2
+import json
+import win32com.client as win32
+
+#Busca dos parâmetros de conexão ao banco de dados PostgreSQL
+db_name = input("Digite o banco de dados desejado: ")
+db_user = input("Digite o nome de usuário do banco de dados: ")
+db_pwd = input("Digite a senha do banco de dados: ")
+db_port = input("Digite a porta do banco de dados: ")
+db_host = input("Digite o host: ")
+
+#Criar conexão com o banco de dados
+conn = psycopg2.connect(dbname=db_name, user=db_user, password=db_pwd, port=db_port, host=db_host) 
+#Opção 2: ler dados acima através de um arquivo externo para proteção dos dados.
+
+results_from_query = pd.read_sql("SELECT * FROM categories", conn)
+print(results_from_query)
+
+# table_names_query = pd.read_sql("SELECT tablename FROM pg_tables WHERE schemaname = 'public'", conn)
+# print(table_names_query)
+
+#Conversão do Dataframe para Json
+results_json_file = results_from_query.to_json()
+
+#Criar arquivo .json
+with open('./Atividade1/backup_data.json', 'w') as fh:
+    fh.write(results_json_file)
+
+conn.close()
+
+#Envio de um email com anexo
+outlook = win32.Dispatch('outlook.application')
+
+email = outlook.CreateItem(0)
+
+email.To = "rtsouza87@gmail.com"
+email.Subject = "Backup BD"
+email.HTMLBody = """
+<p>Bom dia<p>
+
+<p>Segue backup em anexo<p>
+
+<p>Atenciosamente<p>
+"""
+anexo = "./Atividade1/backup_data.json'"
+email.Attachments.Add(anexo)
+
+email.Send()
+print("Email enviado")
+
+
---
+++ Atividade2/connect-to-cosmos.py
@@ -0,0 +1,62 @@
+import os
+import json
+import dotenv
+import pandas as pd
+import psycopg2
+from azure.cosmos import CosmosClient, PartitionKey
+
+#Busca dos parâmetros de conexão ao banco de dados PostgreSQL
+db_name = input("Digite o banco de dados desejado: ")
+db_user = input("Digite o nome de usuário do banco de dados: ")
+db_pwd = input("Digite a senha do banco de dados: ")
+db_port = input("Digite a porta do banco de dados: ")
+db_host = input("Digite o host: ")
+
+try:
+    #Criar conexão com o banco de dados
+    conn = psycopg2.connect(dbname=db_name, user=db_user, password=db_pwd, port=db_port, host=db_host)
+
+    #Consulta SQL para uma tabela - 
+    table_name = input("Digite o nome da tabela a ser consultada: ") #Vide arquivo northwind.sql. No exemplo a tabela foi categories
+    results_from_query = pd.read_sql(f"SELECT * FROM {table_name}", conn)
+    print(results_from_query)
+    results_json_file = results_from_query.to_json()
+    print(results_json_file)
+
+    with open('./Atividade2/backup_data.json', 'w') as fh:
+        fh.write(results_json_file)
+
+    result = json.loads(results_json_file)
+        
+except (Exception, psycopg2.DatabaseError) as error:
+    print("Impossível conectar com o banco de dados")
+finally:
+    conn.close()
+
+
+#Busca das variáveis do sistema
+dotenv.load_dotenv(dotenv.find_dotenv())
+endpoint = os.getenv("COSMOS_ENDPOINT")
+cosmos_key = os.getenv("COSMOS_KEY")
+
+#Criação de um cliente
+client = CosmosClient(url=endpoint, credential=cosmos_key)
+
+#Criação de banco de dados
+database = client.create_database_if_not_exists(id="br_gaap")
+
+
+partitionKeyPath = PartitionKey(path="/categoryId")
+
+#Criação de um container
+container = database.create_container_if_not_exists(
+    id="products", partition_key=partitionKeyPath
+)
+
+container.create_item(results_json_file)
+
+# existingItem = container.read_item(
+#     item="70b63682-b93a-4c77-aad2-65501347265f",
+#     partition_key="61dba35b-4f02-45c5-b648-c6badc0cbd79",
+# )
+
